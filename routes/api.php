65b3738454ff07cffce68616cf61f6361ad3ee03
Mohamad Hesari <mdhesari99@gmail.com>
2021-12-31 03:03:06 +0330

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -5,20 +5,20 @@ use Mdhesari\LaravelCities\Http\Controllers\GeoController;
 
 Route::group(['prefix' => 'geo'], function () {
 
-    Route::get('search/{name}/{parent_id?}', [GeoController::class, 'search');
+    Route::get('search/{name}/{parent_id?}', [GeoController::class, 'search']);
 
-    Route::get('item/{id}', [GeoController::class, 'item');
+    Route::get('item/{id}', [GeoController::class, 'item']);
 
-    Route::get('children/{id}', [GeoController::class, 'children');
+    Route::get('children/{id}', [GeoController::class, 'children']);
 
-    Route::get('parent/{id}', [GeoController::class, 'parent');
+    Route::get('parent/{id}', [GeoController::class, 'parent']);
 
-    Route::get('country/{code}', [GeoController::class, 'country');
+    Route::get('country/{code}', [GeoController::class, 'country']);
 
-    Route::get('countries', [GeoController::class, 'countries');
+    Route::get('countries', [GeoController::class, 'countries']);
 
-    Route::get('ancestors/{id}', [GeoController::class, 'ancestors');
+    Route::get('ancestors/{id}', [GeoController::class, 'ancestors']);
 
-    Route::get('breadcrumbs/{id}', [GeoController::class, 'breadcrumbs');
+    Route::get('breadcrumbs/{id}', [GeoController::class, 'breadcrumbs']);
 
 });
